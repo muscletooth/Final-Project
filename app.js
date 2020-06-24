5f3b79d96263beb532925bbfc96f37a4af9004c4
Ubuntu <ubuntu@ip-172-31-25-65.us-east-2.compute.internal>
2020-06-24 00:43:13 +0000

    latest edition

--- app.js
+++ app.js
@@ -2,45 +2,17 @@
 const express = require("express");
 const app = express();
 
-// Server run
-app.listen("3000", function(){
-    console.log("The Website is online");
-});
-
 // Express Routes
-app.get("/", function(req, res){
-    res.render("homepage.ejs");
+app.get("/Home", function(req, res){
+    res.render("homepage.html");
 });
 
-app.get("/contactUs", function(req, res){
-    res.render("contactUs.ejs");
+app.get("/Resume", function(req, res){
+    res.render("resume.html");
 });
 
-app.get("/game/:gameTitle/:gameCreator", function(req, res){
-    const author = req.params.gameCreator;
-    const title = req.params.gameTitle;
-    res.render("game.ejs", {
-        title: title,
-        author: author
-    });
-});
-
-app.get("/gamelist", function(req, res){
-
-    //  Games List
-    const games = [
-        {title: "Fortnite", author: "Epic Games"},
-        {title: "Battlefield 5", author: "EA"},
-        {title: "Call of Duty: Modern Warfare", author: "Activision"}
-    ]
-
-    res.render("gamelist.ejs", {
-        gamesList: games
-    });
-});
-
-app.get("/hello", function(req, res){
-    res.send("Hello! Welcome to the hello route!");
+app.get("/Contact Us", function(req, res){
+    res.render("contactUs.html");
 });
 
 // Error Route - Must be last
